2cda45802e7ec8eb4df6075e95738174424251db
Diego Torres <diegotc86@gmail.com>
2019-02-28 17:39:35 -0500

    insertion_sort.rb completed

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,18 +1,19 @@
 def insertionSort1(arr)
     value = arr.last
     i = arr.length() -2
-    
+
     until i<0
         if arr[i] > value
             arr[i+1]=arr[i]
+            puts arr.to_s
             i = i-1
         else
-            arr[i] = value
+            arr[i+1] = value
+            puts arr.to_s
+            return
         end
     end
-
-    return arr
 end
 
 array=[1,2,3,5,6,7,8,4]
-puts insertionSort1(array)
+insertionSort1(array)
