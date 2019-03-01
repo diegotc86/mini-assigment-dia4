396a51c58dad73300173b1542ec5ec2dd1529987
Deyvi Conde <iamdeyvi@gmail.com>
2019-03-01 01:21:00 -0500

    Working - Print sorted array, steps are comming

--- selection.rb
+++ selection.rb
@@ -1,16 +1,23 @@
 # You are given an array. Follow the selection sort algorithm 
 # and print the state of the array after “x” iterations have been performed
 
-a = [1,5,10,2,8]
-
-nelem = a.length -1
-
-nelem.times do |i|
-    index_min = i
-
-    (i + 1).upto(nelem) do |j|
-        index_min = j if a[j] < a[index_min]
+def selection_sort(a)
+  n = a.length-1
+  it = 0
+  for i in 0...n
+    min_index=i
+    for j in (i+1)..n
+      if a[j]<a[min_index]
+        min_index=j
+      end
+    temp = a[i]
+    a[i]=a[min_index]
+    a[min_index]=temp
+    it+=1
     end
-
-    #a[i], a[index_min] = a[index_min], a[i] if index_min != i
+  it+=1
+  end
+  return it
 end
+
+print selection_sort([1,190,2,5,3])
